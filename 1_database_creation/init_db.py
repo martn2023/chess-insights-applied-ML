1f6936ba8e1f981e3c6dd19c88d47926cda59ac6
Martin <42924628+martn2022@users.noreply.github.com>
2025-08-12 23:51:42 -0400

    reusable db creation script used to create empty db

---
+++ 1_database_creation/init_db.py
@@ -0,0 +1,12 @@
+import os
+import sqlite3
+
+# SQLite db file should NOT be in this folder, should be one level up in the root
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+db_path = os.path.join(project_root, "chess_insights.sqlite")
+
+
+conn = sqlite3.connect(db_path)
+conn.close()
+
+print(f"✅ Empty database created at: {db_path}")
